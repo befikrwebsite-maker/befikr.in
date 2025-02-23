09dbef47f8f932833369cb30731317fdac4c5da3
Chitransh <chitransh210703@gmail.com>
2025-02-23 14:56:10 +0530

    Update not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -11,9 +11,9 @@ const Errorpage = () => {
           <div className="absolute inset-0 top-[-30%]  flex items-center justify-center text-blue-200 text-[25vw] sm:text-[28vw] md:text-[28vw] lg:text-[28vw] font-extrabold opacity-60">
             404
           </div>
-          <h2 className="relative text-3xl font-bold text-black">WE ARE SORRY, PAGE NOT FOUND!</h2>
+          <h2 className="relative text-3xl font-bold text-black">WE ARE SORRY, PAGE NOT FOUND !</h2>
           <p className="relative mt-2 text-gray-600">
-            THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN REMOVED, HAD ITS NAME CHANGED, OR IS TEMPORARILY UNAVAILABLE.
+            THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN REMOVED, HAD ITS NAME CHANGED, OR IS TEMPORARILY UNAVAILABLE
           </p>
           <Link href="/">
             <button className="relative mt-6 px-6 py-3 text-white bg-blue-600 rounded-md hover:bg-blue-800 transition duration-300 text-lg font-semibold">
