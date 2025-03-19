e42f54a2acf58d80c0a0a483fe50a8e877070894
Animesh Srivastava <animeshsrivastava2003@gmail.com>
2025-03-19 14:55:38 +0530

    (2/2)

--- src/app/components/imageList.js
+++ src/app/components/imageList.js
@@ -1,85 +1,28 @@
 const imageList = [
-    {
-      "name": "Axis Bank",
-      "url": "/logos/axis-bank-logo-1.svg",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Bosch",
-      "url": "/logos/bosch.png",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Crompton",
-      "url": "/logos/crompton-logo.svg",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Domino's Pizza",
-      "url": "/logos/domino-s-pizza-4.svg",
-      "tag": ["Corporate Social Responsibility"]
-    },
-    {
-      "name": "Eveready",
-      "url": "/logos/eveready-removebg-preview.png",
-      "tag": ["Social"]
-    },
-    {
-      "name": "Flipkart",
-      "url": "/logos/flipkart.svg",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "HDFC Bank",
-      "url": "/logos/hdfc-bank-logo.svg",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Hindustan Petroleum",
-      "url": "/logos/Hindustan_Petroleum-Logo.svg",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Indian Oil",
-      "url": "/logos/Indian_Oil.png",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Punjab National Bank",
-      "url": "/logos/punjab-national-bank.svg",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Siemens",
-      "url": "/logos/siemens.png",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Zoho",
-      "url": "/logos/zoho-1.svg",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Embassy",
-      "url": "/logos/embassy.png",
-      "tag": ["Environment"]
-    },
-    {
-      "name": "Kent",
-      "url": "/logos/kent-.png",
-      "tag": ["B2B2C"]
-    },
-    {
-      "name": "Ozone",
-      "url": "/logos/ozone.png",
-      "tag": ["B2B2C"]
-    },
-    {
-        "name": "Jubliant",
-        "url": "/logos/jubliant.png",
-        "tag": ["Social", "Environment"]
-      }
-  ];
+  { "name": "BOSCH Siemens", "url": "/logos/Bsh.png", "tag": ["Environment/ESG Services"] },
+  { "name": "Kent", "url": "/logos/kent-.png", "tag": ["Environment/ESG Services"] },
+  { "name": "Flipkart", "url": "/logos/flipkart.svg", "tag": ["Environment/ESG Services"] },
+  { "name": "True North", "url": "/logos/truenorth_led_logo.jpeg", "tag": ["Environment/ESG Services"] },
+  { "name": "Kotak", "url": "", "tag": ["Environment/ESG Services"] },
+  { "name": "Axis", "url": "/logos/axis-bank-logo-1.svg", "tag": ["Environment/ESG Services"] },
+  { "name": "American Embassy", "url": "/logos/embassy.png", "tag": ["Environment/ESG Services"] },
+  { "name": "HDFC Bank", "url": "/logos/hdfc-bank-logo.svg", "tag": ["Environment/ESG Services", "Social/CSR Services"] },
+  { "name": "Vidya School", "url": "/logos/vidyaschool.jpeg", "tag": ["Social/CSR Services"] },
 
+  { "name": "Crompton", "url": "/logos/crompton-logo.svg", "tag": ["Environment/Circular Economy Services"] },
+  { "name": "Dominos", "url": "/logos/domino-s-pizza-4.svg", "tag": ["Environment/Circular Economy Services"] },
+  { "name": "Attero", "url": "/logos/atteroN.jpg", "tag": ["Environment/Circular Economy Services"] },
+
+  { "name": "Indian Oil", "url": "/logos/Indian_Oil.png", "tag": ["Environment/ESG Services"] },
+  { "name": "HPCL", "url": "/logos/Hindustan_Petroleum-Logo.svg", "tag": ["Environment/ESG Services"] },
+  { "name": "Bank of India", "url": "/logos/bankofindia.jpg", "tag": ["Environment/ESG Services"] },
+  { "name": "PNB", "url": "/logos/punjab-national-bank.svg", "tag": ["Environment/ESG Services"] },
+
+  { "name": "Elcon Drugs & Pharmaceuticals", "url": "", "tag": ["Projects"] },
+  { "name": "Baxy Limited", "url": "/logos/baxy2.jpg", "tag": ["Projects"] },
+
+  { "name": "Eveready", "url": "/logos/eveready-removebg-preview.png", "tag": ["Social/CSR Services"] },
+  { "name": "Jubilant Bhartia Foundation", "url": "/logos/jbf.png", "tag": ["Social/CSR Services"] }
+];
 
 export default imageList;
