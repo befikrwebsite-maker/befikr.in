456e0d795caef40841844796c51ba894314ba132
Animesh Srivastava <animeshsrivastava2003@gmail.com>
2025-03-20 15:25:49 +0530

    changes that even idk

--- src/app/components/ImpactSection.js
+++ src/app/components/ImpactSection.js
@@ -6,22 +6,22 @@ const impacts = [
   {
     title: "Positively Impacting Environment & Sustainability",
     desc: "Enabling Businesses to conserve energy & reduce the negative impact over environment along with creating a social sustainable impact.",
-    image: "/service_img/logo-svg.svg",
+    image: "/impactSection/IMG-20250213-WA0002-removebg-preview.png",
   },
   {
     title: "Organising the Unorganised Business Services",
     desc: "Offering organised & dependable services to businesses thus helping them scale & sustain respective business environment.",
-    image: "/service_img/logo-svg.svg",
+    image: "/impactSection/IMG-20250213-WA0003-removebg-preview.png",
   },
   {
     title: "Pan-India One-Stop Services Company",
     desc: "Setting up a national foot-print of business services bringing uniformity & single-point-of-contact for businesses.",
-    image: "/service_img/logo-svg.svg",
+    image: "/impactSection/IMG-20250213-WA0004-removebg-preview.png",
   },
   {
     title: "In-House Team of Auditors, Engineers & Skilled Professionals",
     desc: "Creating a pool of qualified, skilled & professional Engineers-Handymen along with efficient Project Managers to Key Account Managers.",
-    image: "/service_img/logo-svg.svg",
+    image: "/impactSection/IMG-20250213-WA0005-removebg-preview.png",
   },
 ];
 
@@ -53,7 +53,7 @@ export default function Impact() {
             <img
               src={impacts[index].image}
               alt={impacts[index].title}
-              className="absolute right-0 bottom-0 w-1/4 object-cover"
+              className="absolute right-0 bottom-0 w-1/3 object-cover"
             />
             <div className="flex flex-col justify-center text-lg text-black p-4 z-10">
               <h3 className="text-4xl font-bold">{impacts[index].title}</h3>
