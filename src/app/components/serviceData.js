14a977be654f098cdaa5419dc1b482e0f25bea9b
DivyamShma <107070253+DivyamShma@users.noreply.github.com>
2025-04-29 16:58:29 +0530

    data filling 9-5 job

--- src/app/components/serviceData.js
+++ src/app/components/serviceData.js
@@ -1,3 +1,5 @@
+const { Heading } = require("lucide-react");
+
 const servicesData = {
     "environment/safety-audit/energy-audit": {
         placeholder: "Energy Audit",
@@ -28,12 +30,35 @@ const servicesData = {
     "environment/safety-audit/electrical-safety-audit": {
         placeholder: "Electrical Safety Audir",
         audit: "Electrical Safety Audit",
-        auditdesc: [],
+        auditdesc: ["An electrical safety audit comprehensively assesses electrical systems and infrastructure within a building, facility, or industrial setting to evaluate electrical safety, power efficiency, regulatory compliance, and overall performance for sectors such as Manufacturing, Oil & Gas, mines, Banking and commercial buildings.",
+            "Regular electrical safety audits are recommended, typically every year depending on the nature of business operations and respective regulatory requirements. These regulations are primarily governed by the Central Electricity Authority (CEA) (Measures relating to Safety and Electric Supply) Regulations, 2023. In addition to CEA, the other regulatory bodies & regulations come from State Electricity Boards (SEB), the Bureau of Indian Standards (BIS) & the National Building Code of India (NBC).",
+            "An efficient electrical safety audit, through checks on wiring, equipment, earthing systems, and safety devices, helps minimise the risk of electric shock, fire, and other electrical accidents originating from the electrical infrastructure. It also identifies potential hazards, energy inefficiencies, and compliance issues, ensuring the safety of employees and assets. Prioritizing electrical safety contributes to a secure and efficient working environment for individuals as well as the industry.",
+            "At befikr, as an ESG services company, we offer electrical safety audit services that assist businesses in protecting both people and the planet."
+        ],
         image: "",
         imageScope: "",
-        ArrayAppr: [],
-        ArraySupp: [],
-        scope: ""
+        ArrayAppr: [
+            { title: "Documentation Audit", description: "Checking existing electrical drawings, cyclic maintenance records, and compliance certificates. ", image: "/icons/step1.png" },
+            { title: "Visual inspection", description: "Examining the physical state of electrical installations and equipment. ", image: "/icons/step2.png" },
+            { title: "Testing", description: "Performing electrical tests like insulation resistance, earth continuity, and circuit breaker tripping characteristics. ", image: "/icons/step3.png" },
+            { title: "Reporting", description: "Preparing a detailed report outlining identified safety concerns, recommendations for corrective actions, and compliance status. ", image: "/icons/step3.png" },
+        ],
+        ArraySupp: [
+            { desc: "Preservation of Life and Health by averting accidents & Injuries originating from fire." },
+            { desc: "Following electrical safety guidelines helps reduce the risk of electrical shocks, burns, and injuries to personnel, thus preserving human lives." },
+            { desc: "Following electrical safety guidelines helps reduce the risk of electrical shocks, burns, and injuries to personnel, thus preserving human lives." },
+            { desc: "Identifying potential electrical hazards through regular audits, proactive repairs & improvement activities, manages the risk of damages to equipment & real estate infrastructure, thus preserving the business economy." },
+            { desc: "Compliance with Regulations through compliance verification, documentation, record keeping, employee training & awareness. " },
+            { desc: "Following electrical safety regulations and standards ensures legal compliance & setting up self-governing mechanisms for smooth & safe business operations." }
+        ],
+        scope: "",
+        ArrayInstrumentsAndTechnology: [
+            { title: "Infrared Thermography Cameras", description: "To detect overheating and potential failure points through thermal imaging.", image: "/icons/step1.png" },
+            { title: "Insulation Resistance Testers", description: "For checking the electrical insulation of equipment.", image: "/icons/step2.png" },
+            { title: "Earth Resistance Testers", description: "To measure the resistance of earthing systems.", image: "/icons/step3.png" },
+            { title: "Multimeter and Clamp Meters", description: "For measurement of voltage, current, and resistance.", image: "/icons/step4.png" },
+            { title: "Lux Meters", description: "For assessing lighting levels and ensuring adequate illumination.", image: "/icons/step5.png" },
+        ]
     },
     "environment/circular-economy/e-waste-management": {
         placeholder: "E-Waste Management",
@@ -97,37 +122,86 @@ const servicesData = {
     "environment/circular-economy/reverse-logistics": {
         placeholder: "Reverse Logistics",
         audit: "Reverse Logistics Audit",
-        auditdesc: [],
+        auditdesc: [
+            "Reverse logistics is the process of managing the return of products from end users back to the supply chain, whether for returns, repairs, refurbishments, or recycling. It's a critical part of supply chain management, addressing issues like returns, product recalls, and handling end-of-life products.",
+            "At befikr, as an ESG services company, we provide reverse logistics services for businesses, helping them achieve their environmental goals while protecting both people and the planet through efficient reverse logistics services."
+        ],
         image: "",
         imageScope: "",
-        ArrayAppr: [],
-        ArraySupp: [],
+        ArrayKeyAspects: [
+            {title: "Returns Management", desc: "Handling customer returns, which are a significant aspect of reverse logistics, especially in e-commerce."},
+            {title: "Refurbishment and Repair", desc: "Taking returned products, repairing or refurbishing them, and potentially reselling them."},
+            {title: "Recycling and Disposal", desc: "Properly managing the end-of-life of products, including recycling and disposal, to minimize environmental impact."},
+            {title: "Supply Chain Optimization", desc: "Reverse logistics helps businesses recover value from returned products, reduce waste, and improve overall supply chain efficiency."}
+        ],
+        ArrayExamples: [
+            {title: "Customer Returns", desc: "An e-commerce company processing returns of products ordered online."},
+            {title: "Product Recalls", desc: "A manufacturer recalling a product due to a defect and coordinating its return."},
+            {title: "End-of-Life Products", desc: "Collecting and recycling electronic devices at the end of their useful life."}
+        ],
+        ArrayBenifits: [
+            {title: "Cost Reduction", desc: "By optimizing returns and reducing waste, reverse logistics can significantly lower costs."},
+            {title: "Improved Customer Satisfaction", desc: "Hassle-free returns and efficient handling of returned products can enhance customer loyalty."},
+            {title: "Environmental Sustainability", desc: "Recycling and reusing products can reduce waste and minimize the environmental impact of the supply chain."},
+            {title: "Enhanced Brand Image", desc: "Effective reverse logistics practices can demonstrate a company's commitment to environmental responsibility and customer satisfaction."}
+        ],
         scope: ""
     },
     "governance/testing-inspection-demo/defective-audit": {
         placeholder: "Defective Audit",
         audit: "Defective Product Audit",
-        auditdesc: [],
+        auditdesc: [
+            "A defective product audit service typically involves inspecting, evaluating, and documenting products that fail to meet quality standards or have defects within their promised warranty period.",
+            "This service is valuable for businesses that would like to be:",
+            "1)	fair to the consumers by evaluating the genuineness of defective claims within warranty and settling them basis service levels committed while selling the products.",
+            "2)	aids the entire supply chain credibility including distributors, dealers and retailers through which the product was sold.",
+            "3)	proactive in identifying quality issues in their products, understanding the root causes of defects, and improving product quality avoiding subsequent product quality issues.",
+            "At befikr, as an ESG services company, we provide product testing, inspection & defective audit services for brands facing consumers thus helping companies run well-governed ensuring Corporate Governance protocols are well followed.",
+        ],
         image: "",
         imageScope: "",
-        ArrayAppr: [],
+        ArrayAppr: [
+            {title: "Step 1", desc: "Setting clear criteria for what constitutes a defect (e.g., functional issues, cosmetic defects, safety concerns); and preparing the team to conduct inspections based on these criteria."},
+            {title: "Step 2", desc: "Physical inspection: Checking the products for visible defects, such as damaged packaging, faulty components, or incomplete manufacturing processes."},
+            {title: "Step 3", desc: "Functional inspection: Ensuring that products work as intended (for example, testing electronics, mechanical devices, etc.)."},
+            {title: "Step 4", desc: "Recording findings in detailed reports, which include photographs, descriptions, and categorization of defects."},
+            {title: "Step 5", desc: "Providing a comprehensive report to the client, which includes the number and type of defects found, any trends or patterns, and recommendations for improvement."},
+        ],
         ArraySupp: [],
         scope: ""
     },
     "governance/testing-inspection-demo/installation-demo": {
         placeholder: "Installation Demo",
         audit: "Installation Demo Audit",
-        auditdesc: [],
+        auditdesc: [
+            "Installation and demo services for consumer products offer a comprehensive approach to ensuring a smooth and satisfying customer experience. These services typically include professional installation and demos of the product. The service also demonstrates the product’s features and settings, helping customers understand and use its capabilities.",
+            "At befikr, as an ESG services company, we provide installation & demo services for consumer brand businesses, helping them achieve their governance goals."
+        ],
         image: "",
         imageScope: "",
-        ArrayAppr: [],
-        ArraySupp: [],
+        ArrayAppr: [
+            {title: "Professional Installation", desc: "This involves correctly mounting or placing the product (like a washing machine, dishwasher, refrigerator, etc) and connecting it to the necessary devices."},
+            {title: "Product Demonstration", desc: "Our befikr BROTHER will guide the customer through the product's features, settings, and functionalities, ensuring they understand how to use it effectively."},
+            {title: "Troubleshooting and Assistance", desc: "The service may include troubleshooting any initial issues."},
+            {title: "Optional Add-ons", desc: "Some services may offer additional features like warranty extension, sale of respective product accessories."}
+        ],
+        ArraySupp: [
+            {title: "Peace of mind", desc: "Knowing that the product is installed correctly and set up properly can alleviate the stress and potential issues associated with DIY installation."},
+            {title: "Expert guidance", desc: "A professional can ensure the product is used effectively and answer any initial questions or concerns."},
+            {title: "Time-saving", desc: "Delegating the installation and setup to professionals can save time and effort, especially for consumer products."},
+            {title: "Enhanced customer experience", desc: "A well-executed installation and demo can lead to a more positive customer experience and greater product satisfaction."}
+        ],
         scope: ""
     },
     "social/csr/csr": {
         placeholder: "CSR Services",
         audit: "CSR Audit",
-        auditdesc: [],
+        auditdesc: [
+            "Businesses adopt Corporate Social Responsibility (CSR) activities to behave as ethical and responsible corporate organisations. Through CSR activities, organisations deliver last-mile benefits to different stakeholders of society & value to the stakeholders.",
+            "As an essential part of India's quest for development, inclusive growth is widely recognised reiterating a firm’s commitment to include those sections of society in the growth process which had hitherto remained excluded from the mainstream of development. In line with this national endeavour, CSR is an instrument for integrating social, environmental and human development concerns in the entire value chain of corporate business.",
+            "Profitable businesses have constitutionalised self-regulatory mechanisms to socially contribute to specific sectors of priority & interest to the business group. A well-thought-of CSR initiative eventually benefits the society & the country at large, magnifying various efforts from the government, NGOs as well as the private sector, thus creating a visible impact in the social frame of a country.",
+            "At befikr, as an ESG services company, we assist businesses in achieving their social goals (environmental, philanthropic, socio-economic) through sustainable CSR initiatives."
+        ],
         image: "",
         imageScope: "",
         ArrayAppr: [],
