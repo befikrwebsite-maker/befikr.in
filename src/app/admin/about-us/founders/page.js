8a233501be375790bff89b7066d69ee3ea0cf1f7
DivyamShma <107070253+DivyamShma@users.noreply.github.com>
2025-08-01 18:27:16 +0530

    womp womp

--- src/app/admin/about-us/founders/page.js
+++ src/app/admin/about-us/founders/page.js
@@ -8,7 +8,7 @@ export default function EditFounders() {
   const [newText, setNewText] = useState("");
   const [showTextInput, setShowTextInput] = useState(false);
   const [selectedFounder, setSelectedFounder] = useState("");
-  
+
   const [foundersContentSumit, setFoundersContentSumit] = useState([
     { type: "text", content: "Sumit Srivastava is a passionate fintech entrepreneur since 2016 & the co-founder of befikr.in, one of India’s leading B2B ESG services company founded along with a long-time colleague & friend Chirajay as his co-founder." },
     { type: "text", content: "Today, befikr focuses on Business to Business services focusing on safety, energy & defective audits, circular economy, business sustainability as well as corporate social responsibility services. In addition, befikr is also involved in B2B2C & Institutional services with an experience of over 9 years in serving over a million households, fortune 500 companies, market leading consumer brands and prestigious institutions. " },
@@ -70,7 +70,7 @@ export default function EditFounders() {
                               setShowTextInput(false);
                             }
 
-                          } else if (selectedFounder === "Sumit"){
+                          } else if (selectedFounder === "Sumit") {
                             if (newText.trim() !== "") {
                               setFoundersContentSumit([...foundersContentSumit, { type: "text", content: newText }]);
                               setNewText("");
@@ -144,15 +144,41 @@ export default function EditFounders() {
                     }
                   })}
                 </form>
-                <div className="md:text-4xl font-generalSansRegular"><strong className=" text-companyBlue">Sumit Srivastava</strong> is a passionate fintech entrepreneur since 2016 & the co-founder of befikr.in, one of India’s leading B2B ESG services company founded along with a long-time colleague & friend Chirajay as his co-founder.</div>
+
+                {foundersContentSumit.map((item, index) => {
+                  if (item.type === "text" && index === 0) {
+                    return (
+                      <p className="md:text-4xl font-generalSansRegular"><strong className=" text-companyBlue">Sumit Srivastava</strong>{item.content.replace("Sumit Srivastava","")}</p>
+                    );
+                  } else {
+                    return (
+                      <p className="pt-4 pb-8 ">{item.content}</p>
+                    );
+                    
+                  }
+                })}
+                <div className="items-center h-[0.5px] rounded-full mb-10 bg-companyBlue"></div>
+                {foundersContentChirajay.map((item, index) => {
+                  if (item.type === "text" && index === 0) {
+                    return (
+                      <p className="md:text-4xl font-generalSansRegular"><strong className=" text-companyBlue">Chirajay Sharma</strong>{item.content.replace("Chirajay Sharma","")}</p>
+                    );
+                  } else {
+                    return (
+                      <p className="pt-4 pb-8 ">{item.content}</p>
+                    );
+                    
+                  }
+                })}
+                {/* <p className="md:text-4xl font-generalSansRegular"><strong className=" text-companyBlue">Sumit Srivastava</strong> is a passionate fintech entrepreneur since 2016 & the co-founder of befikr.in, one of India’s leading B2B ESG services company founded along with a long-time colleague & friend Chirajay as his co-founder.</p>
                 <p className="pt-4 pb-8 ">Today, befikr focuses on Business to Business services focusing on safety, energy & defective audits, circular economy, business sustainability as well as corporate social responsibility services. In addition, befikr is also involved in B2B2C & Institutional services with an experience of over 9 years in serving over a million households, fortune 500 companies, market leading consumer brands and prestigious institutions. </p>
                 <p className="pt-4 pb-8">Sumit is an NDIM-Alumni affiliated to the prestigious Guru Gobind Singh Indraprastha University with masters in Finance. Born at Dhanbad, Sumit follows & practises mathematics, business algorithms & business strategy  and loves to spend quality time with the family & two lovely children.</p>
-                <p className="pt-4 pb-8">Today, at befikr he is involved in setting up a strong foundation for the next decade with team & processes to be remembered leaving a strong impact in the spheres of environment, society & safety.</p>
-                <div className="items-center h-[0.5px] rounded-full mb-10 bg-companyBlue"></div>
+                <p className="pt-4 pb-8">Today, at befikr he is involved in setting up a strong foundation for the next decade with team & processes to be remembered leaving a strong impact in the spheres of environment, society & safety.</p> */}
+                {/* {/* <div className="items-center h-[0.5px] rounded-full mb-10 bg-companyBlue"></div>
                 <p className="md:text-4xl font-generalSansRegular"><strong className=" text-companyBlue">Chirajay Sharma</strong> is a passionate marketing entrepreneur since 2016 & the co-founder of befikr.in, one of India’s leading B2B ESG services company founded along with a long-time colleague & friend Sumit as his co-founder.</p>
                 <p className="pt-4 pb-8 ">Today, befikr focuses on Business to Business services focusing on safety, energy & defective audits, circular economy, business sustainability as well as corporate social responsibility services. In addition, befikr is also involved in B2B2C & Institutional services with an experience of over 9 years in serving over a million households, fortune 500 companies, market leading consumer brands and prestigious institutions. </p>
                 <p className="pt-4 pb-8">Chirajay is an IIM Kolkata Alumni with Masters in Pharmacology from Rajiv Gandhi University of Health Sciences, Bangalore & graduation from Rajasthan University. Born at Jodhpur, Chirajay follows the global consumer space with respect to marketing & branding. In his free time he loves exhibiting Interior design closely for family & friends.</p>
-                <p className="pt-4 pb-8">Today, at befikr he is involved in setting up a strong foundation for the next decade as a brand to be remembered leaving a strong impact in the spheres of environment, society & safety.</p>
+                <p className="pt-4 pb-8">Today, at befikr he is involved in setting up a strong foundation for the next decade as a brand to be remembered leaving a strong impact in the spheres of environment, society & safety.</p> */}
               </div>
             </div>
           </div>
